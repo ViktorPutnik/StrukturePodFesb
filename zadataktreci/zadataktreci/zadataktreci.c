df395f34cbca1ec8e3fe0f88df78400b84d470c5
ViktorPutnik <vputni00@fesb,hr>
2024-10-29 15:26:06 +0100

    treci zadatak

--- zadataktreci/zadataktreci/zadataktreci.c
+++ zadataktreci/zadataktreci/zadataktreci.c
@@ -1,174 +1,253 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct _person* positon;
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+typedef struct _person
+{
+    char fname[32], lname[32];
+    int birth_year;
+    positon next;
+} person;
 
-#define MAX_SIZE 50
-#define ERROR -1
-
-struct _struct;
-typedef struct _struct* position;
-
-typedef struct _struct {
-    char name[MAX_SIZE];
-    char lastName[MAX_SIZE];
-    int birthYear;
-    position next;
-}Person;
-
-position CreatePerson(char* name, char* lastName, char birthYear);
-
-int PrependList(position head, char* name, char* lastName, int birthYear);
-
-int PrintList(position first);
-
-int AppendList(position head, char* name, char* lastName, int birthYear);
-
-position FindLast(position head);
-
-int InsertAfter(position previous, position np);
-
-int Delete(position head, char* name);
-
-position findPrevious(position head, char* name);
-
-position FindBySurname(position first, char* surname);
-
-int InsertAtSpot(position current, int Spot,char* name, char* LastName, int birthYear);
+positon create_person(char* fname, char* lname, int birth_year);
+int prepend_list(positon head, char* fname, char* lname, int birth_year);
+int append_list(positon head, char* fname, char* lname, int birth_year);
+int print_list(positon first);
+int insert_after(positon previous, positon to_insert);
+positon find_last(positon head);
+positon find_by_lname(positon first, char* lname);
+int delete_person(positon head, positon to_delete);
+int insert_before(positon next, positon to_insert, positon head);
+int sort_by_lname(positon head);
+int save_list_to_file(positon first, char* filename);
+int read_list_from_file(positon first, char* filename);
 
 int main()
 {
-    Person master =
-    {
-        .name = {0},
-    .lastName = { 0 },
-    .birthYear = {0},
-    .next = NULL
+    person head = { .fname = "", .lname = "", .birth_year = 0, .next = NULL };
+    prepend_list(&head, "Jane", "Doe", 1990);
+    prepend_list(&head, "John", "Janice", 1991);
+    append_list(&head, "Jack", "Jacky", 1992);
+    print_list(&head);
 
-    };
-    /* a.*/
-    PrependList(&master, "Marko", "Maruc", 1999);
+    positon jane = find_by_lname(&head, "Doe");
+    printf("Found: %s %s\n", jane->fname, jane->lname);
 
+    printf("Deleting Jane\n");
+    delete_person(&head, jane);
+    print_list(&head);
 
-    /*b.*/
-    PrintList(master.next);
+    printf("Inserting new person after Jack\n");
+    positon jack = find_by_lname(&head, "Jacky");
+    positon new_person = create_person("Jill", "Doe", 1993);
+    insert_after(jack, new_person);
+    print_list(&head);
 
-    /*c.*/
-    AppendList(&master, "Luka", "Lucic", 2001);
-    PrintList(master.next);
+    printf("Inserting new person before Jack\n");
+    new_person = create_person("Mate", "Mates", 1993);
+    insert_before(jack, new_person, &head);
+    print_list(&head);
 
-    /*d.*/
-    FindBySurname(master.next, "Lucic");
+    printf("Sorting by last name\n");
+    sort_by_lname(&head);
+    print_list(&head);
 
-    /*e.*/
-    Delete(&master, "Marko");
-    PrintList(master.next);
+    printf("Saving list to file\n");
+    save_list_to_file(&head, "zad2.txt");
 
+    person headFile = { .fname = "", .lname = "", .birth_year = 0, .next = NULL };
+    read_list_from_file(&headFile, "zad2.txt");
+    printf("Reading list from file\n");
+    print_list(&headFile);
 
-    /*3. zadatak a.*/
-   
     return 0;
 }
 
-position CreatePerson(char* name, char* lastName, char birthYear)
+
+positon create_person(char* fname, char* lname, int birth_year)
 {
-    position newPerson = NULL;
-    newPerson = (position)malloc(sizeof(Person));
-    if (newPerson == NULL) {
-        printf("Nema mista u memoriji\n");
+    positon new_person = (positon)malloc(sizeof(person));
+    if (!new_person)
+    {
+        printf("Malloc failed in function create_person()\n");
         return NULL;
     }
-    strcpy(newPerson->name, name);
-    strcpy(newPerson->lastName, lastName);
-    newPerson->birthYear = birthYear;
-    newPerson->next = NULL;
-    return newPerson;
+
+    strcpy(new_person->fname, fname);
+    strcpy(new_person->lname, lname);
+    new_person->birth_year = birth_year;
+    new_person->next = NULL;
+
+    return new_person;
 }
 
-int PrependList(position head, char* name, char* lastName, int birthYear) {
-    position newPerson = NULL;
-    newPerson = CreatePerson(name, lastName, birthYear);
-    if (newPerson == NULL) {
-        printf("Nije bilo moguce kreirat novu osobu");
-        return ERROR;
-    }
-    InsertAfter(head, newPerson);
-    return EXIT_SUCCESS;
-}
-
-int PrintList(position first) {
-    position temp = NULL;
-    temp = first;
-    printf("\n");
-    while (temp != NULL) {
-        printf("%s %s %d\n", temp->name, temp->lastName, temp->birthYear);
-        temp = temp->next;
-    }
-    return EXIT_SUCCESS;
-}
-
-int AppendList(position head, char* name, char* lastName, int birthYear) {
-    position newPerson = NULL;
-    newPerson = CreatePerson(name, lastName, birthYear);
-    if (newPerson == NULL) {
-        printf("Nije bilo moguce dodat novu osobu");
-        return ERROR;
-    }
-    position last = NULL;
-    last = FindLast(head);
-    InsertAfter(last, newPerson);
-    return EXIT_SUCCESS;
-}
-
-position FindLast(position head) {
-    position last = NULL;
-    last = head;
-    while (last->next != NULL) {
-        last = last->next;
-    }
-    return last;
-}
-
-int InsertAfter(position previous, position np) {
-    np->next = previous->next;
-    previous->next = np;
-    return EXIT_SUCCESS;
-}
-
-position FindBySurname(position first, char* surname) {
-    position temp = NULL;
-    temp = first;
-    while (temp) {
-        if (strcmp(surname, temp->lastName) == 0) return temp;
-        temp = temp->next;
-    }
-    return NULL;
-}
-
-position findPrevious(position head, char* name) {
-    position current = NULL;
-    current = head;
-    while (current->next != NULL) {
-        if (strcmp((current->next)->name, name) == 0) return current;
-    }
-    return NULL;
-}
-
-int Delete(position head, char* name) {
-    position previous = findPrevious(head, name);
-    position current = previous->next;
-    previous->next = current->next;
-    free(current);
-    return EXIT_SUCCESS;
-}
-
-int InsertAtSpot(position current, int Spot, char* name, char* lastName, int birthYear) 
+int prepend_list(positon head, char* fname, char* lname, int birth_year)
 {
-    position temp = NULL;
-    temp->next = current->next;
-    temp = CreatePerson(name, lastName, birthYear);
-    return EXIT_SUCCESS;
-    
+    positon new_person = create_person(fname, lname, birth_year);
+    if (!new_person)
+    {
+        printf("Malloc failed in function create_person()\n");
+        return -1;
+    }
+
+    new_person->next = head->next;
+    head->next = new_person;
+
+    return 0;
+}
+
+int append_list(positon head, char* fname, char* lname, int birth_year)
+{
+    positon new_person = create_person(fname, lname, birth_year);
+    if (!new_person)
+    {
+        printf("Malloc failed in function create_person()\n");
+        return -1;
+    }
+
+    positon last = find_last(head);
+    last->next = new_person;
+
+    return 0;
+}
+
+int print_list(positon first)
+{
+    positon temp = first->next;
+
+    while (temp)
+    {
+        printf("First name: %s Last name: %s Birth year: %d\n", temp->fname, temp->lname, temp->birth_year);
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+positon find_last(positon head)
+{
+    positon temp = head;
+
+    while (temp->next)
+    {
+        temp = temp->next;
+    }
+
+    return temp;
+}
+
+positon find_by_lname(positon first, char* lname)
+{
+    positon temp = first->next;
+
+    while (temp)
+    {
+        if (strcmp(temp->lname, lname) == 0)
+        {
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+int delete_person(positon head, positon to_delete)
+{
+    positon temp = head;
+
+    while (temp->next != to_delete)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = to_delete->next;
+    free(to_delete);
+
+    return 0;
+}
+
+int insert_after(positon previous, positon to_insert)
+{
+    to_insert->next = previous->next;
+    previous->next = to_insert;
+
+    return 0;
+}
+
+int insert_before(positon next, positon to_insert, positon head)
+{
+    positon temp = head;
+    while (temp->next != next)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = to_insert;
+    to_insert->next = next;
+
+    return 0;
+}
+
+int sort_by_lname(positon head)
+{
+    positon i, j;
+    for (i = head->next; i->next; i = i->next)
+    {
+        for (j = i->next; j; j = j->next)
+        {
+            if (strcmp(i->lname, j->lname) > 0)
+            {
+                char temp[32];
+                strcpy(temp, i->lname);
+                strcpy(i->lname, j->lname);
+                strcpy(j->lname, temp);
+            }
+        }
+    }
+
+    return 0;
+}
+
+int save_list_to_file(positon head, char* filename)
+{
+    FILE* file = fopen(filename, "w");
+    if (!file)
+    {
+        printf("Failed to open file %s\n", filename);
+        return -1;
+    }
+
+    positon temp = head->next;
+    while (temp)
+    {
+        fprintf(file, "%s %s %d\n", temp->fname, temp->lname, temp->birth_year);
+        temp = temp->next;
+    }
+
+    printf("List saved to file %s\n", filename);
+    fclose(file);
+    return 0;
+}
+
+int read_list_from_file(positon head, char* filename)
+{
+    FILE* file = fopen(filename, "r");
+    if (!file)
+    {
+        printf("Failed to open file %s\n", filename);
+        return -1;
+    }
+
+    char fname[32], lname[32];
+    int birth_year;
+    while (fscanf(file, "%s %s %d", fname, lname, &birth_year) == 3)
+    {
+        append_list(head, fname, lname, birth_year);
+    }
+
+    fclose(file);
+    return 0;
 }
